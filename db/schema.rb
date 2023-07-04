eb77e20319ec9636c56dc08dd817b4eee9116d61
Kyle Nichols <kyle.r.nichols@gmail.com>
2023-07-04 21:08:16 +0900

    Restaurant_id migragtion, restaurant controller, and views added

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_07_04_102112) do
+ActiveRecord::Schema[7.0].define(version: 2023_07_04_110907) do
   create_table "restaurants", force: :cascade do |t|
     t.string "name"
     t.string "address"
@@ -25,6 +25,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_07_04_102112) do
     t.text "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "restaurant_id"
   end
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+puts "Removing previous seeds..."
+Restaurant.destroy_all
+puts "All seeds are gone"
+
+puts "Generating new ones..."
+10.times do
+  species = Restaurant::CATEGORIES.sample
+  Restaurant.create(
+    name: Faker::Restaurant.name,
+    address: Faker::Address.street_name,
+    phone_number: Faker::PhoneNumber.cell_phone,
+    category: species
+  )
+end
+puts "New seeds done!"
